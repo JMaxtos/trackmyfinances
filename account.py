289e086a417aecc0e5f67e0a8ea4f14f567f77e7
JMaxtos <jmmso4@iscte-iul.pt>
2025-10-06 00:10:25 +0100

    Fixed How First Row of CSV File is Written

--- account.py
+++ account.py
@@ -24,7 +24,7 @@ class Account :
 
         with open(file,mode='w',newline='',encoding='utf-8') as f:
             writer = csv.writer(f)
-            writer.writerow(['Action', 'Category' , 'Value'])
+            writer.writerow(['Type', 'Category' , 'Value'])
 
 if __name__ == "__main__":
     user = input("Account name: ")
